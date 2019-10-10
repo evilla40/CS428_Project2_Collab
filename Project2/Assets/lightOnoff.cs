43164b5b117670c4a6886bedacd9f99505b8bc47
Conrad21 <Conrad21@gmail.com>
2019-10-10 18:05:32 -0500

    lots of stuff

---
+++ Project2/Assets/lightOnoff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightOnoff : MonoBehaviour
+{
+
+    public GameObject light;
+
+
+    public void lightonoff()
+    {
+
+        if (light.activeInHierarchy)
+        {
+            light.SetActive(false);
+        }
+        else if (!light.activeInHierarchy)
+        {
+            light.SetActive(true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
