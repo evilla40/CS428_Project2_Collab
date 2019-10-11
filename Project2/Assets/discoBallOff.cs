150291848c3ce35947adb023a08a01b48c8f11ed
asaraf1995 <aasthasaraf1995@gmail.com>
2019-10-11 15:32:37 -0500

    More objects (interactions)

---
+++ Project2/Assets/discoBallOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class discoBallOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject discoball;
+
+    public void DiscoBallOff()
+    {
+
+        discoball.SetActive(false);
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
