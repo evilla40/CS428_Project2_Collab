994aa6b395f514d6eda398c4524da0cbb369d9b4
Conrad21 <Conrad21@gmail.com>
2019-10-11 10:24:59 -0500

    Added Some Stuff

---
+++ Project2/Assets/Record.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Record : MonoBehaviour
+{
+
+    public AudioClip AudioClip;
+    public AudioSource AudioSource;
+
+    public void playmusic()
+    {
+
+        if (!AudioSource.isPlaying)
+        {
+            AudioSource.Play();
+        }
+        else if (AudioSource.isPlaying)
+        {
+            AudioSource.Stop();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
