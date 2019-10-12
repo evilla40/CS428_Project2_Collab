2ad2edbf98b0680dc99dc59cfc57f3002f65d81d
Elizabeth <evilla40@uic.edu>
2019-10-12 18:42:50 -0500

    2 Interactables

---
+++ Project2/Assets/Elizabeth's Cubicle/PlayMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Play()
+    {
+        if (source.isPlaying == true)
+        {
+            source.Stop();
+        }
+        else
+            source.PlayOneShot(clip);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
