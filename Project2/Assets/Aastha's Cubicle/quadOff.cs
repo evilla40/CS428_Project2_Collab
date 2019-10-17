75aa23cf326673fecd35c57c586d21023438cd58
asaraf1995 <aasthasaraf1995@gmail.com>
2019-10-17 16:48:15 -0500

    Changes and lot's of changes

---
+++ Project2/Assets/Aastha's Cubicle/quadOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quadOff : MonoBehaviour
+{
+    public GameObject quadoff;
+
+    public void QuadOff()
+    {
+
+        quadoff.SetActive(false);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
