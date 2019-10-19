f988d86cb3d093230d77ef421f71b5e55e73392d
asaraf1995 <aasthasaraf1995@gmail.com>
2019-10-19 16:49:51 -0500

    shrink and grow added

---
+++ Project2/Assets/drawerOn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawerOn : MonoBehaviour
+{
+    public GameObject drawer;
+    public void draweron()
+    {
+        drawer.SetActive(true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
