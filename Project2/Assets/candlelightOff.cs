b6abc0f2fffa6754bb2aad2d39ad9bce83bb42c6
asaraf1995 <aasthasaraf1995@gmail.com>
2019-10-19 00:48:03 -0500

    objects added/ interactables done

---
+++ Project2/Assets/candlelightOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candlelightOff : MonoBehaviour
+{
+    public GameObject candleLight;
+    // Start is called before the first frame update
+    public void candleLinghtOff()
+    {
+        candleLight.SetActive(false);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project2/Assets/switchPainting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchPainting : MonoBehaviour
+{
+    public GameObject stillPainting;
+    public GameObject movingPainting;
+    public GameObject slidingSound;
+    // Start is called before the first frame update
+    public void movePainting()
+    {
+        if (!movingPainting.activeInHierarchy)
+        {
+            slidingSound.SetActive(true);
+            movingPainting.SetActive(true);
+            stillPainting.SetActive(false);
+            
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
