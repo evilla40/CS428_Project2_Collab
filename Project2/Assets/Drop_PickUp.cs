8266886cbad85d8ebf134662341509f6ab9d3a0c
Elizabeth <evilla40@uic.edu>
2019-10-19 18:36:09 -0500

    Floor Model Physics + Interactions Added

---
+++ Project2/Assets/Drop_PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop_PickUp : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Move()
+    {
+        obj.transform.Rotate(0, 0, 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
