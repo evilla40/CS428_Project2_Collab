9cf9786568805b24080fff430961dbc0d3771a2a
asaraf1995 <aasthasaraf1995@gmail.com>
2019-10-19 17:39:38 -0500

    comin in

---
+++ Project2/Assets/Aastha's Cubicle/Scripts/quadOn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quadOn : MonoBehaviour
+{
+        public GameObject quad_on;
+        public void QuadOn()
+        {
+
+            quad_on.SetActive(true);
+
+        }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project2/Assets/grow_and_shrink.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grow_and_shrink : MonoBehaviour
+{
+    public GameObject alias;
+    public float x = 0.1f;
+    public float y = 0.1f;
+    public float z = 0.1f;
+    public float x_pos = 0.1f;
+    public float y_pos = 0.1f;
+    public float z_pos = 0.1f;
+    public void growShrink()
+    {
+        alias.transform.localScale = new Vector3(x, y, z);
+        alias.transform.localPosition = new Vector3(x_pos, y_pos, z_pos);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
