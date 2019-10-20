19d28aee0947341cf7b0a482fa7b4c70c93ae029
Elizabeth <evilla40@uic.edu>
2019-10-19 20:35:55 -0500

    Kibble script updated

--- Project2/Assets/Eat_Kibble.cs
+++ Project2/Assets/Eat_Kibble.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Eat_Kibble : MonoBehaviour
 {
-    public GameObject[] array = new GameObject[]
+    public GameObject[] array;
+   
+    // Start is called before the first frame update
+    void Start()
     {
+       array = new GameObject[]
+        {
         GameObject.Find("Kibble"), GameObject.Find("Kibble1"), GameObject.Find("Sphere1"),
         GameObject.Find("Kibble2"), GameObject.Find("Kibble3"), GameObject.Find("Sphere2"),
         GameObject.Find("Kibble4"), GameObject.Find("Kibble5"), GameObject.Find("Sphere3"),
         GameObject.Find("Kibble6"), GameObject.Find("Kibble7")
-    };
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        };
     }
 
     public void Eat()
