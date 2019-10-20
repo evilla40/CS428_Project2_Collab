9aa6552136a9de3108833029f72a4b782cfb42f1
Elizabeth <evilla40@uic.edu>
2019-10-19 20:58:43 -0500

    Final update

---
+++ Project2/Assets/Drop_PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop_PickUp : MonoBehaviour
+{
+    public int z;
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Move()
+    {
+        obj.transform.Rotate(0, 0, z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project2/Assets/Eat_Kibble.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat_Kibble : MonoBehaviour
+{
+    public GameObject[] array;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+       array = new GameObject[]
+        {
+        GameObject.Find("Kibble"), GameObject.Find("Kibble1"), GameObject.Find("Sphere1"),
+        GameObject.Find("Kibble2"), GameObject.Find("Kibble3"), GameObject.Find("Sphere2"),
+        GameObject.Find("Kibble4"), GameObject.Find("Kibble5"), GameObject.Find("Sphere3"),
+        GameObject.Find("Kibble6"), GameObject.Find("Kibble7")
+        };
+    }
+
+    public void Eat()
+    {
+        int n;
+        while (true)
+        {
+            n = Random.Range(0, 10);
+            if (array[n].activeSelf == true)
+            {
+                array[n].SetActive(false);
+                break;
+            }
+            else continue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
